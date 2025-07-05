697f98a8479b9e372f924af443e2b40df6774da4
Kovach Dmitriy <96537647+kodmig90@users.noreply.github.com>
2025-07-05 16:46:32 +0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# Запуск Telegram-бота
+# Запуск Telegram-бота (в фоне)
 python3 main.py &
 
-# Запуск аптайм-сервера
-python3 uptime.py &
+# Запуск аптайм-сервера (в foreground)
+python3 uptime.py
